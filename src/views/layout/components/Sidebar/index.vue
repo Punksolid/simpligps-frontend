421c98367cf4222826761ab75cf31b6560fb5606
Alan Herrera <alanherreraldg@gmail.com>
2018-11-01 17:21:42 -0700

    Cambios Sidebar menu
    Cambios bg

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -5,9 +5,9 @@
       :default-active="$route.path"
       :collapse="isCollapse"
       mode="vertical"
-      background-color="#304156"
-      text-color="#bfcbd9"
-      active-text-color="#409EFF"
+      background-color="ffffff"
+      text-color="#727272"
+      active-text-color="#000000"
     >
       <sidebar-item v-for="route in routes" :key="route.name" :item="route" :base-path="route.path"/>
     </el-menu>
@@ -33,3 +33,7 @@ export default {
   }
 }
 </script>
+<style scoped>
+
+
+</style>
