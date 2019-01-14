111da57a1ede44cde4fdb074bf291a480ed73cb6
Jose Palazuelos <punksolid@gmail.com>
2019-01-14 00:31:28 -0700

    avances menu lee menu y submenu falta formato y funcionalidad

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,86 +1,37 @@
 <template>
+  <!-- el elemento li va SIEMPRE, sea link normal o sea elemento para submenú -->
   <li
-    v-if="!item.hidden&&item.children"
-    class="nav-active"
-    v-bind:class="{
-      active: isActive(item.path),
-      'nav-hover': isHovering,
-      'nav-parent': hasChilds(item),
-      'active': isExpanded
-      }"
-    @mouseover="isHovering = true"
-    @mouseout="isHovering = false"
-    @click="expandCollapse"
+    class="nav nav-active"
+    v-if="!item.hidden"
+    :class="{ 'nav-parent':hasChildren(item)}"
   >
-    <!-- ENLACE INDIVIDUAL -->
 
-    <template
-      v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
-      <app-link :to="resolvePath(onlyOneChild.path)" class="elemento">
-        <div
-          :index="resolvePath(onlyOneChild.path)"
-          :class="{'submenu-title-noDropdown':!isNest}">
-          <!--CUANDO COINCIDEN isActive regresa true, con v-bind se pone la clase extra -->
-          <!--{{ item.path }}-->
-          <!--{{ this.$route.path }}-->
-          <!--{{ isActive(item.path) }}-->
-          <item
-            v-if="onlyOneChild.meta"
-            :icon="onlyOneChild.meta.icon||item.meta.icon"
-            :title="onlyOneChild.meta.title"/>
-        </div>
-      </app-link>
-    </template><!-- FIN - ENLACE INDIVIDUAL -->
-
-
-
-    <template v-else :index="resolvePath(item.path)">
-
-      <a class="elementos">
-        <item v-if="item.meta" :icon="item.meta.icon" :title="item.meta.title" />
-      </a>
-
-      <ul class="children collapse">
-        <template v-for="child in item.children" v-if="!child.hidden">
-          <item
-            v-if="child.children&&child.children.length>0"
-            :is-nest="true"
-            :item="child"
-            :key="child.path"
-            :base-path="resolvePath(child.path)"
-            class="nest-menu" />
-          <app-link v-else :to="resolvePath(child.path)" :key="child.name">
-            <li :index="resolvePath(child.path)">
-              <item v-if="child.meta" :icon="child.meta.icon" :title="child.meta.title" />
-            </li>
-          </app-link>
-        </template>
-      </ul>
-
-    </template>
-
+    <a class="testing">
+      <div index="/users" class="submenu-title-noDropdown">
+        <i class="icon-users"></i>
+        <span>
+        <router-link :to="item.path" class="testing" active-class="active">
+          {{ item.name }} - {{ hasChildren(item) }}
+        </router-link>
+        </span>
+      </div>
+    </a>
+    <ul v-if="hasChildren(item)">
+      <li v-for="child in item.children" :key="id">{{ child.name }}</li>
+    </ul>
   </li>
 </template>
 
 <script>
-  import path from 'path'
-  import Item from './Item'
-  import AppLink from './Link'
+  // import path from 'path'
 
   export default {
     name: 'SidebarItem',
-    components: { Item, AppLink },
     props: {
-      isHovering: false,
-      isExpanded: false,
       // route object
       item: {
         type: Object,
         required: true
-      },
-      isNest: {
-        type: Boolean,
-        default: false
       },
       basePath: {
         type: String,
@@ -89,72 +40,22 @@
     },
     data() {
       return {
-        onlyOneChild: null
+        onlyOneChild: null // @todo innescesario pero no supe como poner un default
       }
     },
     methods: {
-      hasOneShowingChild(children, parent) {
-        const showingChildren = children.filter(item => {
-          if (item.hidden) {
-            return false
-          } else {
-            // Temp set(will be used if only has one showing child)
-            this.onlyOneChild = item
-            return true
-          }
-        })
-
-        // When there is only one child router, the child router is displayed by default
-        if (showingChildren.length === 1) {
-          return true
-        }
-
-        // Show parent if there are no child router to display
-        if (showingChildren.length === 0) {
-          this.onlyOneChild = { ...parent, path: '', noShowingChildren: true }
-          return true
-        }
-
-        return false
-      },
-      resolvePath(routePath) {
-        return path.resolve(this.basePath, routePath)
-      },
-      isActive(itemPath) {
-        if (itemPath === this.$route.path) {
-          return true
-        }
-        return false
-      },
-      hasChilds(item) {
-        if (item.children.length > 1) {
-          return true
-        } else {
-          return false
-        }
-      },
-      expandCollapse() {
-        if (this.isExpanded) {
-          this.isExpanded = false
-        } else {
-          this.isExpanded = true
-        }
-      },
-      parentPath() {
-        const parent_path = this.$route.path.split('/')[1]
-        return parent_path
-      },
-      parentPathActive(item) {
-        if (item.path === this.parentPath()) {
+      hasChildren(item) {
+        if (item.hasOwnProperty('children')) {
+          console.log('si tiene children')
           return true
         } else {
           return false
         }
       }
-
     }
   }
 </script>
+
 <style rel="stylesheet/scss" lang="scss" scoped>
   .sidebar-collapsed .sidebar .sidebar-inner .nav-sidebar > li > a > div > span:not(.arrow) {
     left: 40px;
