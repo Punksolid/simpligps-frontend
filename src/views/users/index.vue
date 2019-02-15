7590faf41e6173331e97aaf02464476103e19dbb
Jose G. Rivera <josepablogr@gmail.com>
2019-02-15 02:32:52 -0700

    User Search Form
    Multiple inputs for Search Users.

--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -1,16 +1,29 @@
 <template>
   <el-row class="panel p-10">
 
-    <el-row type="flex" justify="space-between">
+    <el-row class="searchBar">
 
-      <el-col :span="5">
+      <el-col class="m-b-5" :xl="4" :sm="4" :xs="24">
         <el-button type="primary" @click="openDialog" icon="fas fa-user-plus p-r-10">Create user</el-button>
       </el-col>
-      <el-col class="t-right" :span="14">
-        <el-button-group>
-          <el-button>Clear date filter</el-button>
-          <el-button>Clear all filters</el-button>
-        </el-button-group>
+
+      <el-col :xl="20" :sm="20" :xs="24">
+        <el-form class="dis-flex" v-model="search">
+          <el-form-item>
+          <el-input placeholder="Name" v-model="search.name" @keyup.enter.native="handleFilter"/>
+          </el-form-item>
+          <el-form-item>
+          <el-input placeholder="Lastname" v-model="search.lastname" @keyup.enter.native="handleFilter"/>
+          </el-form-item>
+          <el-form-item>
+          <el-input placeholder="Email" v-model="search.email" @keyup.enter.native="handleFilter">
+            <el-button slot="append" icon="fas fa-search" @click="handleFilter"></el-button>
+          </el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button icon="fas fa-trash-alt" plain @click="fetchUserPage"></el-button>
+          </el-form-item>
+        </el-form>
       </el-col>
 
     </el-row>
@@ -56,18 +69,18 @@
         <el-table-column
           label="Operations"
           fixed="right"
-          width="190">
+          width="130">
           <template slot-scope="scope">
             <el-button
               size="mini"
-              @click="handleUpdate(scope.$index, usersListData)">
-              Edit
+              @click="handleUpdate(scope.$index, usersListData)"
+              icon="fas fa-edit">
             </el-button>
             <el-button
               @click.native.prevent="deleteRow(scope.$index, usersListData)"
               type="danger"
-              size="mini">
-              Delete
+              size="mini"
+              icon="fas fa-trash">
             </el-button>
           </template>
         </el-table-column>
@@ -101,6 +114,11 @@
     data() {
       return {
         usersListData: null,
+        search: {
+          name: '',
+          lastname: '',
+          email: ''
+        },
         listLoading: true,
         elementToUpdate: {},
         usersListPage: {
@@ -128,7 +146,6 @@
           type: 'warning'
         }).then(() => {
           deleteUser(userListData[index].id)
-          // this.fetchUsersList()
           this.fetchUserPage()
           this.$message({
             type: 'success',
@@ -156,7 +173,7 @@
       },
       fetchUserPage() {
         this.listLoading = true
-        console.log(this.usersListPage)
+        this.search = {}
         usersList(this.usersListPage).then(response => {
           this.usersListPage = response.data.meta
           this.usersListPage.page = response.data.meta.current_page
@@ -173,6 +190,15 @@
         this.usersListPage.page = val
         this.fetchUserPage()
       },
+      handleFilter() {
+        this.listLoading = true
+        usersList(this.search).then(response => {
+          this.usersListPage = response.data.meta
+          this.usersListPage.page = response.data.meta.current_page
+          this.usersListData = response.data.data
+          this.listLoading = false
+        })
+      },
       fetchUsersList() {
         this.listLoading = true
         usersList(this.usersListData).then(response => {
@@ -188,28 +214,20 @@
     created() {
       this.fetchUsersList()
       this.fetchUserPage()
-    },
-    data() {
-      return {
-        usersListData: null,
-        listLoading: true,
-        elementToUpdate: {},
-        usersListPage: {
-          page: 0,
-          from: 0,
-          last_page: 0,
-          per_page: 15,
-          to: 0,
-          total: 0
-        },
-        form: {},
-        dialogStatus: '',
-        titleDialog: {
-          update: 'Edit User',
-          create: 'Create User'
-        },
-        dialogVisible: false
-      }
     }
   }
 </script>
+<style lang="scss" scoped>
+  .searchBar .dis-flex {
+    justify-content: flex-end;
+    .el-form-item {
+      margin-bottom: 5px;
+      padding: 0px 5px;
+    }
+  }
+  @media screen and (max-width: 468px) {
+    .dis-flex {
+      flex-direction: column;
+    }
+  }
+</style>
