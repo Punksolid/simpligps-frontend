8fa2258408272cf1951795f33a4d23c2d24f691d
Jose Palazuelos <punksolid@gmail.com>
2019-08-20 23:31:41 -0600

    test

---
+++ tests/unit/views/NewTrip.spec.js
@@ -0,0 +1,9 @@
+import NewTrip from 'src/views/catalogs/trips/newtrip.vue'
+
+describe('Newtrip.vue', () => {
+  const wrapper = mount(NewTrip)
+
+  it('can press a create trip planification button', () => {
+
+  })
+})
