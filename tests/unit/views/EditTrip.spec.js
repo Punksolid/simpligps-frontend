78fd07d27dafd2f21846d4ddd31330719ac3ce0a
Jose Palazuelos <punksolid@gmail.com>
2019-09-02 03:55:20 -0600

    gitflow-feature-stash: block-delete-intermediate-point

---
+++ tests/unit/views/EditTrip.spec.js
@@ -0,0 +1,192 @@
+import { mount, shallowMount } from '@vue/test-utils'
+import NewTrip from '@/views/catalogs/trips/newtrip.vue'
+import TripMock from '@/mock/trips.js'
+import nock from 'nock'
+import { Select } from 'element-ui'
+
+describe('Newtrip.vue', () => {
+  let wrapper
+  const tripResponse = {
+      'id': 32,
+      'rp': 'Prueba Salida',
+      'folio': null,
+      'invoice': 'este es el invoice nuevo',
+      'client_id': 1,
+      'client_name': 'StarkIndustries',
+      'origin_name': 'CTTMX',
+      'destination_name': 'Mente Interactiva',
+      'stops': 0,
+      'mon_type': 'Monitoring',
+      'carrier_id': 1,
+      'truck_tract_id': 14,
+      'truck_name': 'Persona Chema',
+      'operator_id': 2,
+      'scheduled_load': '2019-08-06 22:39:00',
+      'scheduled_departure': '2019-08-07 22:39:00',
+      'scheduled_arrival': '2019-08-08 22:39:00',
+      'scheduled_unload': '2019-08-09 22:39:00',
+      'real_departure': null,
+      'real_arrival': null,
+      'bulk': null,
+      'tag': null,
+      'convoy_id': null,
+      'georoute_ref': '17471233_7',
+      'tags': [{ 'name': 'etiquetaazul', 'slug': 'etiquetaazul' }],
+      'truck': {
+        'id': 14,
+        'name': 'Persona Chema',
+        'plate': 'Bicicleta',
+        'model': 'Desconocido',
+        'internal_number': '111111111',
+        'brand': 'Desconocido',
+        'gps': null,
+        'color': 'Azul',
+        'carrier_id': 1,
+        'device_id': null,
+        'operator_id': null,
+        'current_operator': { 'id': 14, 'name': 'OperatorToDelete' }
+      },
+      'carrier': {
+        'id': 5,
+        'carrier_name': 'Douglas-Bruen',
+        'contact_name': 'Prof. Bryana Block',
+        'phone': '+1.515.951.8546',
+        'email': 'shyanne.mohr@gutmann.com'
+      },
+      'operator': { 'id': 2, 'name': 'ComercializadoraSAdeCVV' },
+      'origin': {
+        'id': 20,
+        'name': 'CTTMX',
+        'person_in_charge': 'Daniel',
+        'address': 'Rosales',
+        'phone': '66666666666',
+        'high_risk': 0,
+        'geofence_ref': '17471233_1',
+        'checkpoint_id': 80,
+        'at_time': '2019-08-06 22:39:00',
+        'exiting': '2019-08-07 22:39:00',
+        'real_at_time': '2019-08-22 07:25:48',
+        'real_exiting': '2019-08-22 07:25:48',
+        'status': 2
+      },
+      'destination': {
+        'id': 22,
+        'name': 'Mente Interactiva',
+        'person_in_charge': 'Heber Martinez',
+        'address': 'asdfasd',
+        'phone': '876182763187283',
+        'high_risk': 0,
+        'geofence_ref': '17471233_6',
+        'checkpoint_id': 87,
+        'at_time': '2019-08-08 22:39:00',
+        'exiting': '2019-08-09 22:39:00',
+        'real_at_time': null,
+        'real_exiting': null,
+        'status': 0
+      },
+      // 'intermediates': [],
+      'intermediates': [{
+        'id': 2,
+        'name': 'Clay Champlin',
+        'person_in_charge': 'Nya Zboncak',
+        'address': '712 Hane Tunnel Suite Ritchieville, LA 44484',
+        'phone': '(461) 816-7986',
+        'high_risk': 0,
+        'geofence_ref': null,
+        'checkpoint_id': 300,
+        'at_time': '2019-09-04 07: 30: 00',
+        'exiting': '2019-09-04 07: 30: 00',
+        'real_at_time': '2019-09-06 09:31:00',
+        'real_exiting': null,
+        'status': 0
+      }, {
+        'id': 7,
+        'name': 'Oda Kessler PhD',
+        'person_in_charge': 'Jarred Murphy',
+        'address': '44872 Lorena Mall Apt. 888\nCarrollchester, WA 45880',
+        'phone': '252.795.4517 x558',
+        'high_risk': 1,
+        'geofence_ref': null,
+        'checkpoint_id': 301,
+        'at_time': '2019-09-06 09: 31: 00',
+        'exiting': '2019-09-06 09: 31: 00',
+        'real_at_time': null,
+        'real_exiting': null,
+        'status': 0
+      }],
+      'trailers': [],
+      'client': {
+        'id': 1,
+        'description': 'Company Description',
+        'company_name': 'StarkIndustries',
+        'address': 'Sky Tower #123',
+        'city': 'Culiacan',
+        'state': 'Sinaloa',
+        'phone': '6677220033',
+        'email': 'stark@industries.com',
+        'person_name': '6',
+        'status': true,
+        'email_frequency': 1
+      }
+    }
+
+  beforeEach(() => {
+    wrapper = mount(NewTrip, {
+      propsData: {
+        trip: tripResponse
+      }
+    })
+  })
+  //
+  afterEach(() => {
+    wrapper.destroy()
+
+  })
+
+  it('can press a create trip planification button and put loading', () => {
+    const wrapper_create = mount(NewTrip)
+    const submit = wrapper_create.find('#submit')
+    expect(submit.html()).toContain('Create Trip Planification')
+
+    expect(wrapper_create.vm.loadings.general).toBe(false)
+
+    submit.trigger('click')
+
+    // expect(wrapper_create.vm.loadings.general).toBe(true)
+    expect(submit.vm.loading).toBe(true)
+  })
+
+  it('should disable the origin to be modified when real_at_time is set on origin', () => {
+    const wrapper = mount(NewTrip, {
+      propsData: {
+        trip: tripResponse
+      }
+    })
+
+    expect(wrapper.vm.locks.origin).toBe(true)
+  })
+
+  it('should disable the delete intermediate button when real_at_time is set', function() {
+    const wrapper_stub = shallowMount(NewTrip, {
+      propsData: {
+        trip: tripResponse
+      }
+    })
+
+    expect(wrapper_stub.find('.intermediates-fields').find('.el-icon-remove').vm.disabled).toBeTruthy()
+    expect(wrapper.vm.locks.intermediates).toContain(300)
+    expect(wrapper.vm.locks.intermediates).not.toContain(301)
+  })
+
+  it('should show intermediates filled', function() {
+    const wrapper_stub = shallowMount(NewTrip, {
+      propsData: {
+        trip: tripResponse
+      }
+    })
+    console.log(wrapper_stub.find('.intermediates-fields').find(Select).vm.value)
+    expect(wrapper_stub.find('.el-icon-remove').exists()).toBeTruthy()
+    expect(wrapper_stub.vm.form.intermediates[0].checkpoint_id).toEqual(tripResponse.intermediates[0].checkpoint_id)
+
+  })
+})
--- tests/unit/views/NewTrip.spec.js
+++ tests/unit/views/NewTrip.spec.js
@@ -5,132 +5,34 @@ import { tripDetails } from '@/api/trips'
 import nock from 'nock'
 import { Select } from 'element-ui'
 
-describe('Newtrip.vue', () => {
+describe('NewTrip.vue', () => {
   let wrapper
-  const tripResponse = {
-      'id': 32,
-      'rp': 'Prueba Salida',
-      'folio': null,
-      'invoice': 'este es el invoice nuevo',
-      'client_id': 1,
-      'client_name': 'StarkIndustries',
-      'origin_name': 'CTTMX',
-      'destination_name': 'Mente Interactiva',
-      'stops': 0,
-      'mon_type': 'Monitoring',
-      'carrier_id': 1,
-      'truck_tract_id': 14,
-      'truck_name': 'Persona Chema',
-      'operator_id': 2,
-      'scheduled_load': '2019-08-06 22:39:00',
-      'scheduled_departure': '2019-08-07 22:39:00',
-      'scheduled_arrival': '2019-08-08 22:39:00',
-      'scheduled_unload': '2019-08-09 22:39:00',
-      'real_departure': null,
-      'real_arrival': null,
-      'bulk': null,
-      'tag': null,
-      'convoy_id': null,
-      'georoute_ref': '17471233_7',
-      'tags': [{ 'name': 'etiquetaazul', 'slug': 'etiquetaazul' }],
-      'truck': {
-        'id': 14,
-        'name': 'Persona Chema',
-        'plate': 'Bicicleta',
-        'model': 'Desconocido',
-        'internal_number': '111111111',
-        'brand': 'Desconocido',
-        'gps': null,
-        'color': 'Azul',
-        'carrier_id': 1,
-        'device_id': null,
-        'operator_id': null,
-        'current_operator': { 'id': 14, 'name': 'OperatorToDelete' }
-      },
-      'operator': { 'id': 2, 'name': 'ComercializadoraSAdeCVV' },
-      'origin': {
-        'id': 20,
-        'name': 'CTTMX',
-        'person_in_charge': 'Daniel',
-        'address': 'Rosales',
-        'phone': '66666666666',
-        'high_risk': 0,
-        'geofence_ref': '17471233_1',
-        'checkpoint_id': 80,
-        'at_time': '2019-08-06 22:39:00',
-        'exiting': '2019-08-07 22:39:00',
-        'real_at_time': '2019-08-22 07:25:48',
-        'real_exiting': '2019-08-22 07:25:48',
-        'status': 2
-      },
-      'destination': {
-        'id': 22,
-        'name': 'Mente Interactiva',
-        'person_in_charge': 'Heber Martinez',
-        'address': 'asdfasd',
-        'phone': '876182763187283',
-        'high_risk': 0,
-        'geofence_ref': '17471233_6',
-        'checkpoint_id': 87,
-        'at_time': '2019-08-08 22:39:00',
-        'exiting': '2019-08-09 22:39:00',
-        'real_at_time': null,
-        'real_exiting': null,
-        'status': 0
-      },
-      'intermediates': [],
-      'trailers': [],
-      'client': {
-        'id': 1,
-        'description': 'Company Description',
-        'company_name': 'StarkIndustries',
-        'address': 'Sky Tower #123',
-        'city': 'Culiacan',
-        'state': 'Sinaloa',
-        'phone': '6677220033',
-        'email': 'stark@industries.com',
-        'person_name': '6',
-        'status': true,
-        'email_frequency': 1
-      }
-    }
 
+  it('can show form with empty fields', () => {
+      wrapper = mount(NewTrip)
 
-  // beforeEach(() => {
-  //   wrapper = mount(NewTrip, {
-  //     propsData: {
-  //       trip: tripResponse
-  //     }
-  //   })
-  // })
-  //
-  // afterEach(() => {
-  //   wrapper.destroy()
-  //
-  // })
-
-  it('can press a create trip planification button and put loading', () => {
-    const wrapper_create = mount(NewTrip)
-    const submit = wrapper_create.find('#submit')
-    expect(submit.html()).toContain('Create Trip Planification')
-
-    expect(wrapper_create.vm.loadings.general).toBe(false)
-
-    submit.trigger('click')
-
-    // expect(wrapper_create.vm.loadings.general).toBe(true)
-    expect(submit.vm.loading).toBe(true)
+      expect(wrapper.vm.$data.form.georoute_ref).toBeNull()
+      expect(wrapper.vm.$data.form.origin_id).toBeNull()
   })
 
-  it('should disable the origin to be modified when real_at_time is set on origin', () => {
-    const wrapper = mount(NewTrip, {
-      propsData: {
-        trip: tripResponse
-      }
-    })
-
-
-    expect(wrapper.vm.locks.origin).toBe(true)
+  it('should display a plus button to add intermediates', function() {
+    wrapper = shallowMount(NewTrip)
+    const plus_button = wrapper.find('#add-intermediate')
+    expect(plus_button.exists()).toBe(true)
 
   })
+
+  it('should not show intermediates fields by default', function() {
+    wrapper = shallowMount(NewTrip)
+
+    expect(wrapper.find('.intermediates-fields').exists()).toBeFalsy()
+
+    const add_intermediate_button = wrapper.find('#add-intermediate')
+
+    add_intermediate_button.vm.$emit('click')
+
+    expect(wrapper.find('.intermediates-fields').exists()).toBeTruthy()
+
+  })
+
 })
